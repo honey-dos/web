0d414ad790a7215d9041e946c4848af243e14e31
Tyler <tyler.evans116@gmail.com>
2018-11-08 15:26:55 -0700

    update installation and running

--- npmStart.sh
+++
@@ -1,3 +0,0 @@
-# Install NPM and start the server, it will run side-by-side with the dotnet API server for speed
-npm install --prefix HoneyDo.Web/ClientApp
-npm start --prefix HoneyDo.Web/ClientApp
--- start.sh
+++
@@ -1,4 +0,0 @@
-# restores, builds, and runs the project
-dotnet restore
-dotnet build
-dotnet run --project HoneyDo.Web/HoneyDo.Web.csproj
