1c60a0cb781a274d23817d7b51349f83772e2037
Tyler <tyler.evans116@gmail.com>
2018-11-28 00:35:35 -0700

    #8 add "Add Task" button

--- HoneyDo.Web/ClientApp/src/pages/Tasks.tsx
+++ HoneyDo.Web/ClientApp/src/pages/Tasks.tsx
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 import TasksList from "../components/Tasks/List";
+import { Theme, createStyles, withStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
 
-const Tasks = () => (
-  <div>
-    <TasksList />
-  </div>
-);
+const styles = ({ spacing }: Theme) =>
+  createStyles({
+    button: {
+      margin: spacing.unit,
+      float: "right"
+    }
+  });
 
-export default Tasks;
+interface TasksProps {
+  classes: { [key: string]: any };
+}
+
+class Tasks extends Component<TasksProps, {}> {
+  static propTypes = {
+    classes: PropTypes.object.isRequired
+  };
+  render() {
+    const { classes } = this.props;
+    return (
+      <div>
+        <TasksList />
+        <Button
+          className={classes.button}
+          onClick={() => console.log("add task")}>
+          Add Task
+        </Button>
+      </div>
+    );
+  }
+}
+
+export default withStyles(styles)(Tasks);
