f7f93783da5b1e98df5b8bac1cc74abd99d63564
nate <nathan.borup@gmail.com>
2019-01-04 20:14:45 -0700

    #32 Update home page

--- HoneyDo.Web/ClientApp/src/pages/Home.tsx
+++ HoneyDo.Web/ClientApp/src/pages/Home.tsx
@@ -4,16 +4,16 @@ import Typography from "@material-ui/core/Typography";
 const Home = () => (
   <div>
     <Typography variant="body1" color="inherit">
-      Ever have issues getting your significant other to do choirs around the
+      Ever have issues getting your significant other to do chores around the
       living space? Hate having to bug them all the time to get it done? Have a
-      hard time keeping track off all the reptitive choirs around the house?
+      hard time keeping track of all the repetitive chores around the house?
     </Typography>
-    <Typography variant="body1" color="inherit">
+    <Typography variant="h4" color="inherit" align="center">
       Honey-Dos is for you!
     </Typography>
     <Typography variant="body1" color="inherit">
-      Honey-Dos is the app to keep track of all your tasks, choirs, bills and
-      shopping lists in one easy to manage place. Easily share the workload with
+      Honey-Dos is the app to keep track of all your tasks, chores, bills and
+      shopping lists in one easy-to-manage place. Easily share the workload with
       your significant other, roomate(s), children or anyone else you share the
       living space with.
     </Typography>
