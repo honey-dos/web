16afb139f04dbf1b3fd4e6f42e80d915dcb300ce
Tyler <tyler.evans116@gmail.com>
2019-01-07 21:53:53 -0700

    #34 refactor dueDate and complete endpoints

--- HoneyDo.Web/Controllers/TodoController.cs
+++ HoneyDo.Web/Controllers/TodoController.cs
@@ -8,6 +8,7 @@ using HoneyDo.Web.Models;
 using HoneyDo.Domain.Interfaces;
 using HoneyDo.Domain.Specifications.Todos;
 using Microsoft.AspNetCore.Authorization;
+using System.ComponentModel.DataAnnotations;
 
 namespace HoneyDo.Web.Controllers
 {
@@ -189,18 +190,40 @@ namespace HoneyDo.Web.Controllers
         }
 
         /// <summary>
-        /// (Un)Complete a todo.
+        /// Complete a todo.
         /// </summary>
         /// <param name="id">Id of todo to (un)complete.</param>
         /// <returns></returns>
         /// <response code="200">Returns the updated Todo.</response>
         /// <response code="400">No todo found.</response>
         /// <response code="401">Don't have access to delete this todo.</response>
-        [HttpPut("{id}/complete"), HttpDelete("{id}/complete")]
+        [HttpPut("{id}/complete")]
         [ProducesResponseType(200)]
         [ProducesResponseType(400)]
         [ProducesResponseType(401)]
         public async Task<ActionResult<Todo>> Complete(Guid id)
+        {
+            return await SetCompletion(id, true);
+        }
+
+        /// <summary>
+        /// Uncomplete a todo.
+        /// </summary>
+        /// <param name="id">Id of todo to (un)complete.</param>
+        /// <returns></returns>
+        /// <response code="200">Returns the updated Todo.</response>
+        /// <response code="400">No todo found.</response>
+        /// <response code="401">Don't have access to delete this todo.</response>
+        [HttpDelete("{id}/complete")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        public async Task<ActionResult<Todo>> Uncomplete(Guid id)
+        {
+            return await SetCompletion(id, false);
+        }
+
+        private async Task<ActionResult<Todo>> SetCompletion(Guid id, bool isComplete)
         {
             var todo = await _todoRepository.Find(new TodoById(id));
             if (todo == null)
@@ -214,8 +237,7 @@ namespace HoneyDo.Web.Controllers
                 return Unauthorized();
             }
 
-            var isPut = Request.Method == "PUT";
-            if (isPut)
+            if (isComplete)
             {
                 todo.Complete();
             }
@@ -229,19 +251,48 @@ namespace HoneyDo.Web.Controllers
         }
 
         /// <summary>
-        /// Add or remove due date.
+        /// Add due date to todo.
         /// </summary>
-        /// <param name="id">Id of todo to modify due date.</param>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     PUT /api/todos/{id}
+        ///     "2018-11-13T23:53:09.651Z"
+        ///
+        /// </remarks> 
+        /// <param name="id">Id of todo to add due date.</param>
         /// <param name="dueDate">Due date value to add to todo.</param>
         /// <returns></returns>
         /// <response code="200">Returns the updated Todo.</response>
-        /// <response code="400">No todo found, or dueDate not given when adding to todo (PUT only).</response>
+        /// <response code="400">No todo found, or dueDate not given.</response>
         /// <response code="401">Don't have access to delete this todo.</response>
-        [HttpPut("{id}/due"), HttpDelete("{id}/due")]
+        [HttpPut("{id}/due")]
         [ProducesResponseType(200)]
         [ProducesResponseType(400)]
         [ProducesResponseType(401)]
-        public async Task<ActionResult<Todo>> Due(Guid id, [FromBody] DateTime? dueDate)
+        public async Task<ActionResult<Todo>> AddDueDate(Guid id, [FromBody, Required] DateTime dueDate)
+        {
+            return await UpdateDueDate(id, dueDate);
+        }
+
+        /// <summary>
+        /// Remove due date from todo.
+        /// </summary>
+        /// <param name="id">Id of todo to remove due date.</param>
+        /// <returns></returns>
+        /// <response code="200">Returns the updated Todo.</response>
+        /// <response code="400">No todo found.</response>
+        /// <response code="401">Don't have access to delete this todo.</response>
+        [HttpDelete("{id}/due")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        public async Task<ActionResult<Todo>> RemoveDueDate(Guid id)
+        {
+            return await UpdateDueDate(id, null);
+        }
+
+        private async Task<ActionResult<Todo>> UpdateDueDate(Guid id, DateTime? dueDate)
         {
             var todo = await _todoRepository.Find(new TodoById(id));
             if (todo == null)
