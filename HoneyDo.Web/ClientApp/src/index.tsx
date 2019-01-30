21c202eaab76b771502b6f1e074d6217dba2a6cd
Tyler <tyler.evans116@gmail.com>
2019-01-29 21:55:31 -0700

    #53 #48 fix: use stage auth for dev and stage remove scope dep

--- HoneyDo.Web/ClientApp/src/index.tsx
+++ HoneyDo.Web/ClientApp/src/index.tsx
@@ -12,10 +12,12 @@ const baseUrl =
 const rootElement = document.getElementById("root");
 
 const config = {
-  apiKey: "AIzaSyA8IvTRwITWjIeSvc0iW5Yl63Q5j6Wyuk0",
-  authDomain: "honey-dos.firebaseapp.com",
-  databaseURL: "https://honey-dos.firebaseio.com",
-  storageBucket: "honey-dos.appspot.com"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID
 };
 firebase.initializeApp(config);
 
