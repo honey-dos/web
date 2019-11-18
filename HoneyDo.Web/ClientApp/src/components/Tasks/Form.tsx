ae5c2139f20faaf33c4127bf2503f3ef82f4e6cf
Tyler Evans <tyler.evans116@gmail.com>
2019-11-18 14:26:52 -0700

    refactor: class to function component

--- HoneyDo.Web/ClientApp/src/components/Tasks/Form.tsx
+++ HoneyDo.Web/ClientApp/src/components/Tasks/Form.tsx
@@ -1,12 +1,4 @@
-import React, {
-  Component,
-  ChangeEvent,
-  RefObject,
-  KeyboardEvent,
-  useState,
-  useRef,
-  useEffect
-} from "react";
+import React, { useState, useRef, useEffect, FunctionComponent } from "react";
 import { Theme, createStyles, withStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
@@ -37,6 +29,10 @@ const styles = ({ spacing, breakpoints }: Theme) =>
         flexBasis: 320
       }
     },
+    addDueDate: {
+      margin: spacing(),
+      flexBasis: 160
+    },
     buttonContainer: {
       display: "flex",
       flexDirection: "row-reverse"
@@ -45,123 +41,107 @@ const styles = ({ spacing, breakpoints }: Theme) =>
 
 interface TaskFormProps {
   task?: Task;
-  classes: { [key: string]: any };
+  classes: { [key: string]: string };
   onSave: (task: TaskFormModel) => void;
   onCancel: () => void;
   onDelete?: () => void;
   width: string;
 }
 
-interface TaskFormState {
-  name: string;
-  hasError: boolean;
-  dueDate?: Date;
-}
-
-class TaskForm extends Component<TaskFormProps, TaskFormState> {
-  nameInput: RefObject<HTMLInputElement>;
-
-  constructor(props: TaskFormProps) {
-    super(props);
-    const name = (props.task && props.task.name) || "";
-    const dueDate = (props.task && props.task.dueDate) || undefined;
-    this.state = { name, hasError: false, dueDate };
-    this.nameInput = React.createRef();
-  }
-
-  componentDidMount() {
-    if (this.nameInput && this.nameInput.current) {
-      this.nameInput.current.focus();
+const useComponentDidMount = (func: () => void) => {
+  const [hasMounted, setMounted] = useState(false);
+  useEffect(() => {
+    if (!hasMounted && func) {
+      setMounted(true);
+      func();
     }
-  }
+  }, [hasMounted, func]);
+};
 
-  handleNameChange = () => (event: ChangeEvent<HTMLInputElement>) => {
-    const name = event.target.value;
-    this.setState({ name, hasError: false });
-  };
-
-  handleDateChange = (dueDate: Date | null) => {
-    this.setState({ dueDate: dueDate || undefined });
-  };
-
-  saveOnEnter = () => (event: KeyboardEvent<HTMLDivElement>) => {
-    if (event.keyCode === 13) {
-      this.handleSave();
-    }
-  };
-
-  handleSave = () => {
-    const { name, dueDate } = this.state;
+const TaskFormFunction: FunctionComponent<TaskFormProps> = (
+  props: TaskFormProps
+) => {
+  const { classes, task, onCancel, onDelete, width, onSave } = props;
+  const [name, updateName] = useState((props.task && props.task.name) || "");
+  const [dueDate, updateDueDate] = useState<Date | undefined>(
+    (props.task && props.task.dueDate) || undefined
+  );
+  const [hasError, updateError] = useState(false);
+  const nameRef = useRef({ focus: () => {} });
+  useComponentDidMount(() => {
+    nameRef && nameRef.current && nameRef.current.focus();
+  });
+  const handleSave = () => {
     if (!name || name.trim().length === 0) {
-      this.setState({ hasError: true });
+      updateError(true);
       return;
     }
     const newTask: TaskFormModel = {
       name,
       dueDate
     };
-    this.props.onSave(newTask);
+    onSave(newTask);
   };
 
-  render() {
-    const { classes, task, onCancel, onDelete, width } = this.props;
-    const { hasError, dueDate } = this.state;
-    return (
-      <div>
-        <div className={classes.inputContainer}>
-          <TextField
-            id="name"
-            label="Name"
-            value={this.state.name}
-            onChange={this.handleNameChange()}
-            onKeyUp={this.saveOnEnter()}
+  return (
+    <div>
+      <div className={classes.inputContainer}>
+        <TextField
+          id="name"
+          label="Name"
+          value={name}
+          onChange={event => updateName(event.target.value)}
+          // onKeyUp={this.saveOnEnter()}
+          margin="dense"
+          variant="outlined"
+          fullWidth
+          inputRef={nameRef}
+          error={hasError}
+          helperText={hasError ? "Name is required." : null}
+          className={classes.input}
+        />
+        {dueDate ? (
+          <KeyboardDateTimePicker
+            disablePast
+            variant="inline"
+            inputVariant="outlined"
+            format="yyyy/MM/dd HH:mm"
             margin="dense"
-            variant="outlined"
-            fullWidth
-            inputRef={this.nameInput}
-            error={hasError}
-            helperText={hasError ? "Name is required." : null}
-            className={classes.input}
+            className={[classes.input, classes.dateInput].join(" ")}
+            fullWidth={width === "xs" || width === "sm"}
+            label="Due Date"
+            value={dueDate}
+            onChange={date => updateDueDate(date || undefined)}
+            KeyboardButtonProps={{
+              "aria-label": "change date"
+            }}
+            // onKeyUp={this.saveOnEnter()}
           />
-          {dueDate ? (
-            <KeyboardDateTimePicker
-              disablePast
-              variant="inline"
-              inputVariant="outlined"
-              format="yyyy/MM/dd HH:mm"
-              margin="dense"
-              className={[classes.input, classes.dateInput].join(" ")}
-              fullWidth={width === "xs" || width === "sm"}
-              label="Due Date"
-              value={dueDate}
-              onChange={this.handleDateChange}
-              KeyboardButtonProps={{
-                "aria-label": "change date"
-              }}
-              onKeyUp={this.saveOnEnter()}
-            />
-          ) : (
-            <Button>Add Due Date</Button>
-          )}
-        </div>
-        <div className={classes.buttonContainer}>
-          <Button className={classes.button} onClick={() => this.handleSave()}>
-            Save
+        ) : (
+          <Button
+            className={classes.addDueDate}
+            onClick={() => updateDueDate(new Date())}>
+            Add Due Date
           </Button>
-          <Button className={classes.button} onClick={() => onCancel()}>
-            Cancel
-          </Button>
-          {task ? (
-            <Button
-              className={classes.button}
-              onClick={() => (onDelete ? onDelete() : null)}>
-              Delete
-            </Button>
-          ) : null}
-        </div>
+        )}
       </div>
-    );
-  }
-}
+      <div className={classes.buttonContainer}>
+        <Button className={classes.button} onClick={() => handleSave()}>
+          Save
+        </Button>
+        <Button className={classes.button} onClick={() => onCancel()}>
+          Cancel
+        </Button>
+        {task ? (
+          <Button
+            className={classes.button}
+            onClick={() => (onDelete ? onDelete() : null)}>
+            Delete
+          </Button>
+        ) : null}
+      </div>
+    </div>
+  );
+};
 
-export default withStyles(styles)(withWidth()(TaskForm));
+export default withStyles(styles)(withWidth()(TaskFormFunction));
