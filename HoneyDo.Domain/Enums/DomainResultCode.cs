294786d0cd4d327d498fc3fa99104b348865fa48
Tyler Evans <tyler.evans116@gmail.com>
2019-11-30 15:34:14 -0700

    domain result code

---
+++ HoneyDo.Domain/Enums/DomainResultCode.cs
@@ -0,0 +1,10 @@
+namespace HoneyDo.Domain.Enums
+{
+    public enum DomainResultCode
+    {
+        NoError = 0,
+        NotFound = 1,
+        NotAuthorized = 2,
+        InvalidArgument = 3
+    }
+}
